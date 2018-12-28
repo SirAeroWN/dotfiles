90ae35c411b527dc02b4f345ed69bcdbedc1f5ea
norv <sicklyserver@gmail.com>
2018-12-28 12:17:50 -0500

    mostly reorganizing bash aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -9,18 +9,12 @@ alias a='echo "------------Your aliases------------";alias'
 alias sa='source ~/.dotfiles/bash/aliases.sh;echo "Bash aliases sourced."'
 alias bp='vim ~/.bash_profile'
 
+# source program specific aliases
+for f in $DOTHOME/bash/aliases/*.sh; do
+  source "$f"
+done
 
 # Git Stuff
-alias co='git checkout'
-alias st='git status'
-alias yolo='git status && git add -A && git commit -m "I do what I want" && git push origin master'
-
-# add bash autocomplete to git aliases (hopefully)
-if [ -f /usr/share/bash-completion/completions/git ]; then
-  source /usr/share/bash-completion/completions/git
-  __git_complete co _git_checkout
-  __git_complete st _git_status
-fi
 
 # Filesystem Stuff
 # alias l='ls -F'
@@ -30,23 +24,9 @@ alias ..='cd ..'
 alias ...='cd ../../'
 
 # using exa instead of ls
-alias l='exa'
-alias la='exa --all'
-alias ll='exa --all --long'
-alias lt='exa --tree --level=5'
-alias lg='exa --all --long --git'
 
 
-### vim ###
-alias v="vim"
-alias vi="vim"
-alias vip="vim -p"
-alias svi="sudo vim"
-alias vic="vim ~/.vim_runtime/my_configs.vim"
-alias va="vim $DOTHOME/bash/aliases.sh"
 
-### neovim ###
-alias nv="nvim"
 
 
 ### updates ###
@@ -63,8 +43,6 @@ alias howdoi="howdoi --color --all"
 
 
 ### maven ###
-alias mvnci="mvn clean install"
-alias mvnstci="mvn -DskipTests=True clean install"
 
 
 ### mac specific ###
---
+++ bash/aliases/git.aliases.sh
@@ -0,0 +1,14 @@
+################################################################################
+#######                       BASH ALIASES                               #######
+################################################################################
+
+alias co='git checkout'
+alias st='git status'
+alias yolo='git status && git add -A && git commit -m "I do what I want" && git push origin master'
+
+# add bash autocomplete to git aliases (hopefully)
+if [ -f /usr/share/bash-completion/completions/git ]; then
+  source /usr/share/bash-completion/completions/git
+  __git_complete co _git_checkout
+  __git_complete st _git_status
+fi
