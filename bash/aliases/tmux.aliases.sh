c3b23e452d6c9d32c025d6c9b94d3397f1713b3d
norv <sicklyserver@gmail.com>
2019-02-26 01:11:53 -0500

    mostly tmux

--- bash/aliases/tmux.aliases.sh
+++ bash/aliases/tmux.aliases.sh
@@ -2,3 +2,7 @@ alias txl='tmux ls'
 alias txn='tmux new -s'
 alias txa='tmux a -t'
 alias txrm='tmux new -s remappy'
+txp () {
+  tmux ls | awk '{print $1 $8}' | sed s/:/\ started\ at\ / | yank;
+  tmux a -t "$( pbpaste )";
+}
